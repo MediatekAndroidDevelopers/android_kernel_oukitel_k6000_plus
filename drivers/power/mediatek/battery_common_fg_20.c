f9183e31020c205e03698c6e88311feb295bf7dd
DerTeufel <dominik-kassel@gmx.de>
2017-11-15 09:40:21 +0100

    battery: export current_now and voltage_now

--- drivers/power/mediatek/battery_common_fg_20.c
+++ drivers/power/mediatek/battery_common_fg_20.c
@@ -270,6 +270,8 @@ int g_present_smb = 0;
 static int cmd_discharging = -1;
 static int adjust_power = -1;
 static int suspend_discharging = -1;
+static int current_now = -1;
+static int voltage_now = -1;
 
 /* ////////////////////////////////////////////////////////////////////////////// */
 /* FOR ANDROID BATTERY SERVICE */
@@ -313,6 +315,8 @@ struct battery_data {
 	int capacity_smb;
 	int present_smb;
 	int adjust_power;
+	int current_now;
+	int voltage_now;
 };
 
 static enum power_supply_property wireless_props[] = {
@@ -350,6 +354,8 @@ static enum power_supply_property battery_props[] = {
 	POWER_SUPPLY_PROP_present_smb,
 	/* ADB CMD Discharging */
 	POWER_SUPPLY_PROP_adjust_power,
+	POWER_SUPPLY_PROP_CURRENT_NOW,
+	POWER_SUPPLY_PROP_VOLTAGE_NOW,
 };
 
 struct timespec batteryThreadRunTime;
@@ -696,6 +702,12 @@ static int battery_get_property(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_adjust_power:
 		val->intval = data->adjust_power;
 		break;
+	case POWER_SUPPLY_PROP_CURRENT_NOW:
+		val->intval = data->current_now;
+		break;
+	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
+		val->intval = data->voltage_now;
+		break;
 
 	default:
 		ret = -EINVAL;
@@ -765,6 +777,8 @@ static struct battery_data battery_main = {
 	.present_smb = 0,
 	/* ADB CMD discharging */
 	.adjust_power = -1,
+	.current_now = -1,
+	.voltage_now = -1,
 #else
 	.BAT_STATUS = POWER_SUPPLY_STATUS_NOT_CHARGING,
 	.BAT_HEALTH = POWER_SUPPLY_HEALTH_GOOD,
@@ -783,6 +797,8 @@ static struct battery_data battery_main = {
 	.present_smb = 0,
 	/* ADB CMD discharging */
 	.adjust_power = -1,
+	.current_now = -1,
+	.voltage_now = -1,
 #endif
 };
 
@@ -1751,6 +1767,16 @@ static void battery_update(struct battery_data *bat_data)
 		battery_log(BAT_LOG_CRTI, "adjust_power=(%d)\n", adjust_power);
 	}
 
+	if (current_now != -1) {
+		bat_data->current_now = current_now;
+		battery_log(BAT_LOG_CRTI, "current_now=(%d)\n", current_now);
+	}
+
+	if (voltage_now != -1) {
+		bat_data->voltage_now = voltage_now;
+		battery_log(BAT_LOG_CRTI, "voltage_now=(%d)\n", voltage_now);
+	}
+
 #ifdef DLPT_POWER_OFF_EN
 #ifndef DISABLE_DLPT_FEATURE
 	if (bat_data->BAT_CAPACITY <= DLPT_POWER_OFF_THD) {
@@ -2163,6 +2189,8 @@ void mt_battery_GetBatteryData(void)
 	temperatureV = battery_meter_get_tempV();
 	temperatureR = battery_meter_get_tempR(temperatureV);
 	ZCV = battery_meter_get_battery_zcv();
+	current_now = ICharging * 1000;
+	voltage_now = bat_vol * 1000;
 
 	BMT_status.ICharging =
 	    mt_battery_average_method(BATTERY_AVG_CURRENT, &batteryCurrentBuffer[0], ICharging,
